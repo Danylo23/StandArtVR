6c740ca8eb8572fe46024ab9071a1ca0344e4af7
Danylo23 <44833735+Danylo23@users.noreply.github.com>
2020-05-26 23:35:39 +0300

    Game bullet Babyuk VR

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public float shootingForce = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+		GetComponent<Rigidbody> ().AddForce (Vector3.forward * shootingForce);
+
+    }
+		
+}
